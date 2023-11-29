e39e522af56e9836a64f60917f3d3cb0e7e597cc
twlster <twlster>
2023-11-29 15:33:58 +0000

    Initial commit

---
+++ src/main/java/com/udemy/micronautjwt/Application.java
@@ -0,0 +1,10 @@
+package com.udemy.micronautjwt;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/udemy/micronautjwt/MicronautUdemyBbddJwtTest.java
@@ -0,0 +1,21 @@
+package com.udemy.micronautjwt;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautUdemyBbddJwtTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
